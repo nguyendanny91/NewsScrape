d5979ab63ffd5fa6a754813e5cf5017b1c38a910
Danny Noowen <nguyendanny91@gmail.com>
2019-01-20 22:32:14 -0800

    final

--- server.js
+++ server.js
@@ -34,7 +34,11 @@ app.use(express.json());
 app.use(express.static("public"));
 
 // Connect to the Mongo DB
-mongoose.connect("mongodb://localhost/NewsScrape", { useNewUrlParser: true });
+
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/NewsScrape";
+
+
+mongoose.connect(MONGODB_URI), { useNewUrlParser: true };
 
 // Routes
 
