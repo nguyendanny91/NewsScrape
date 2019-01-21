852c54ebfe2f1bce69f2c04a33e169e8dc18307d
Danny Noowen <nguyendanny91@gmail.com>
2019-01-20 22:27:43 -0800

    homework

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,5 +1,58 @@
 $(document).ready(function () {
 
+$(".scrape").click(function() {
+    $.ajax({ url: "/scrape", method: "GET" })
+    .then(function (data) {
+        console.log(data)       
+    })
+    .then(
+        function() {
+            location.reload();
+        }
+    );
+});
+
+$(".unscrape").click(function() {
+    $.ajax({ url: "/unscrape", method: "GET" })
+    .then(function (data) {
+        console.log(data)       
+    })
+    .then(
+        function() {
+            location.reload();
+        }
+    );
+});
+
+$(".savebtn").click(function() {
+    var buttonid = $(this).attr("btnid")
+
+    var title = $("[titleid="+buttonid+"]").val()
+    var text = $("[areaid="+buttonid+"]").val()
 
 
-})
+  // Run a POST request to change the note, using what's entered in the inputs
+  $.ajax({
+    method: "POST",
+    url: "/articles/" + buttonid,
+    data: {
+      // Value taken from title input
+      title: title,
+      // Value taken from note textarea
+      body: text
+    }
+  })
+    // With that done
+    .then(function(data) {
+      // Log the response
+      console.log(data);
+      // Empty the notes section
+      alert("Note saved!")
+    });
+
+    
+});
+
+
+
+});
--- server.js
+++ server.js
@@ -42,6 +42,7 @@ mongoose.connect("mongodb://localhost/NewsScrape", { useNewUrlParser: true });
 
 app.get("/", function(req, res) {
     db.Article.find({})
+    .populate("note")
     .then(function(data) {
         var hbsObject = {
         article: data
@@ -99,6 +100,14 @@ app.get("/scrape", function(req, res) {
   });
 });
 
+  app.get("/unscrape", function(req, res) {
+    db.Article.remove({})
+    .then(function(dbTodo) {
+      res.json(dbTodo);
+      console.log(dbTodo)
+    });
+  });
+
 // Route for getting all Articles from the db
 app.get("/articles", function(req, res) {
   // Grab every document in the Articles collection
@@ -113,21 +122,21 @@ app.get("/articles", function(req, res) {
     });
 });
 
-// Route for grabbing a specific Article by id, populate it with it's note
-app.get("/articles/:id", function(req, res) {
-  // Using the id passed in the id parameter, prepare a query that finds the matching one in our db...
-  db.Article.findOne({ _id: req.params.id })
-    // ..and populate all of the notes associated with it
-    .populate("note")
-    .then(function(dbArticle) {
-      // If we were able to successfully find an Article with the given id, send it back to the client
-      res.json(dbArticle);
-    })
-    .catch(function(err) {
-      // If an error occurred, send it to the client
-      res.json(err);
-    });
-});
+// // Route for grabbing a specific Article by id, populate it with it's note
+// app.get("/articles/:id", function(req, res) {
+//   // Using the id passed in the id parameter, prepare a query that finds the matching one in our db...
+//   db.Article.findOne({ _id: req.params.id })
+//     // ..and populate all of the notes associated with it
+//     .populate("note")
+//     .then(function(dbArticle) {
+//       // If we were able to successfully find an Article with the given id, send it back to the client
+//       res.json(dbArticle);
+//     })
+//     .catch(function(err) {
+//       // If an error occurred, send it to the client
+//       res.json(err);
+//     });
+// });
 
 // Route for saving/updating an Article's associated Note
 app.post("/articles/:id", function(req, res) {
